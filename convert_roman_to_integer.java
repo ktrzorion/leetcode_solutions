cd010844412e5701f5026e75472027126569c516
ktr_zorion <111456400+ktrzorion@users.noreply.github.com>
2023-06-06 01:21:18 +0530

    Update convert_roman_to_integer.java

--- convert_roman_to_integer.java
+++ convert_roman_to_integer.java
@@ -1,1 +1,17 @@
-
+class Solution{
+  public int romanToInt(String s){
+    // Writing string according to their values in descending order.
+    String symbol = "MDCLXVI";
+    // Alloting values to letter by index.
+    int{} value = {1000, 500, 100, 50, 10, 5, 1}
+    
+      //The loop for iterating Roman String.
+      for(int i = s.length()-1, i>=0; i--){
+        int current = value[symbol.indexOf(s.charAt(i))]
+          if(past <= current) num += current;
+          else (num -= current);
+          past = current;
+      }
+    return num;
+  }
+}
